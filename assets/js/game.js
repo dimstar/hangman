4abe3989d77954c3a1ad5110a7d075d90a6af16e
dimstar <kylermagee@gmail.com>
2018-02-15 23:16:15 -0800

    Added psuedo code to cover game architecture we'll use 3 object, one global and 2 nested which will handle frontend and input respectfully

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
 // Main game file for all logic for hangman
+
+// Create a global object which holds the game state
+    // key track of score
+    // key track of words
+    // keep track of guessed letters
+    // interact with user input object
+
+// create a global object which is responsible for taking inputs from the user
+    // listen to and handle jey strokes
+    // process the input & send values to the game state object
+
+// create a global object which is responsible for transforming the frontend 
+    // get all frontend elements
+    // recieve values from the game state object
+    // transform the frontend elements based on game state
